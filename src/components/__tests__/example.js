57e3c8cf4493692f77760ecc89215764adc1f664
Bryan Goss Ginger <bcgoss@gmail.com>
2020-06-04 08:16:07 -0700

    Reformats with prettier

--- src/components/__tests__/example.js
+++ src/components/__tests__/example.js
@@ -1,12 +1,12 @@
-import React from "react"
-import { render } from "@testing-library/react"
+import React from 'react';
+import { render } from '@testing-library/react';
 
 // You have to write data-testid
-const Title = () => <h1>Gatsby is awesome!</h1>
+const Title = () => <h1>Gatsby is awesome!</h1>;
 
-test("Displays the correct title", () => {
-  const { getByText } = render(<Title />)
+test('Displays the correct title', () => {
+  const { getByText } = render(<Title />);
   // Assertion
-  expect(getByText("Gatsby is awesome!")).toBeInTheDocument()
+  expect(getByText('Gatsby is awesome!')).toBeInTheDocument();
   // --> Test will pass
-})
+});
