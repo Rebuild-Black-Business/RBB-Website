cc131ab33016d0f9a2b0c7d2dcae9260de7c5687
Mike Bifulco <mbifulco@live.com>
2020-06-04 09:30:25 -0400

    run prettier

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,11 +1,14 @@
-import React from 'react'
-import { Helmet } from 'react-helmet'
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export default function Layout({ children }) {
   return (
     <>
-      <Helmet defaultTitle="Rebuild Black Businesses" titleTemplate="%s - Rebuild Black Businesses" />
+      <Helmet
+        defaultTitle="Rebuild Black Businesses"
+        titleTemplate="%s - Rebuild Black Businesses"
+      />
       {children}
     </>
-  )
+  );
 }
