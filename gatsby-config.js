c90c4ecad96d01f863f9ae8108f774407a1da5de
Mike Bifulco <mbifulco@live.com>
2020-06-04 09:40:31 -0400

    stub metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,32 @@
+
+// this will probably be www.rebuildblackbusiness.com
+const BASE_SITE_URL = process.env.BASE_SITE_URL;
+
 module.exports = {
+  siteMetadata: {
+    title: `Rebuild Black Business`,
+    // eslint-disable-next-line max-len
+    description: ``,
+    author: ``,
+    organization: {
+      name: '',
+      url: '',
+      logo: '',
+    },
+    social: {
+      twitter: ''
+      fbAppID: '',
+      instagram: '',
+      github: '',
+    },
+    logo: {
+      src: '',
+      alt: '',
+    },
+    image_url: `${BASE_SITE_URL}/icons/icon-512x512.png`, // used for RSS feed image
+    logoText: 'Rebuild Black Business',
+    siteUrl: BASE_SITE_URL,
+  },
   plugins: [
     {
       resolve: "gatsby-plugin-chakra-ui",
