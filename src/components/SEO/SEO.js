bd9862a641e9990511b8f72c17b65072b11ff9b2
Mike Bifulco <mbifulco@live.com>
2020-06-04 11:23:48 -0400

    simplify component

--- src/components/SEO/SEO.js
+++ src/components/SEO/SEO.js
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet';
 import { useStaticQuery, graphql } from 'gatsby';
 import PropTypes from 'prop-types';
 
-const SEO = ({ postData, frontmatter = {}, postImage, isBlogPost }) => {
+const SEO = ({ canonicalUrl }) => {
   const { site } = useStaticQuery(
     graphql`
       {
@@ -28,33 +28,27 @@ const SEO = ({ postData, frontmatter = {}, postImage, isBlogPost }) => {
           }
         }
       }
-    `,
+    `
   );
 
   const { siteMetadata: seo } = site;
 
-  const postMeta =
-    frontmatter || postData.childMarkdownRemark.frontmatter || {};
-
-  const title = postMeta?.title || seo?.title;
-  const description = postMeta?.description || seo?.description;
-  const image = postImage ? `${seo.siteUrl}${postImage}` : seo?.image;
-  const url = postMeta.slug ? `${seo.siteUrl}/${postMeta.slug}/` : seo?.siteUrl;
-
-  // const datePublished = isBlogPost ? postMeta.datePublished : false;
+  const title = seo?.title;
+  const description = seo?.description;
+  const image = seo?.image;
+  const url = seo?.siteUrl;
 
   return (
     <React.Fragment>
-      <Helmet defaultTitle={seo.title} titleTemplate={`%s - ${seo.title}`}>
+      <Helmet defaultTitle={title} titleTemplate={`%s - ${title}`}>
         {/* General tags */}
         <title>{title}</title>
         <meta name="description" content={description} />
         <meta name="image" content={image} />
-        <link rel="canonical" href={url} />
+        <link rel="canonical" href={canonicalUrl} />
 
         {/* OpenGraph tags */}
         <meta property="og:url" content={url} />
-        {isBlogPost ? <meta property="og:type" content="article" /> : null}
         <meta property="og:title" content={title} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={image} />
@@ -78,20 +72,7 @@ const SEO = ({ postData, frontmatter = {}, postImage, isBlogPost }) => {
 };
 
 SEO.propTypes = {
-  isBlogPost: PropTypes.bool,
-  postData: PropTypes.shape({
-    childMarkdownRemark: PropTypes.shape({
-      frontmatter: PropTypes.any,
-      excerpt: PropTypes.any,
-    }),
-  }),
-  postImage: PropTypes.string,
-};
-
-SEO.defaultProps = {
-  isBlogPost: false,
-  postData: { childMarkdownRemark: {} },
-  postImage: null,
+  canonicalUrl: PropTypes.string,
 };
 
 export default SEO;
