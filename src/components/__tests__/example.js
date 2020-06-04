da60d740358724235698f5873a1b1c41b13e7b1b
Bryan Goss <bcgoss@users.noreply.github.com>
2020-06-03 20:59:49 -0700

    Makes example more user-centric
    Co-authored-by: Brent Clark <brentmclark@gmail.com>

--- src/components/__tests__/example.js
+++ src/components/__tests__/example.js
@@ -2,11 +2,11 @@ import React from "react"
 import { render } from "@testing-library/react"
 
 // You have to write data-testid
-const Title = () => <h1 data-testid="hero-title">Gatsby is awesome!</h1>
+const Title = () => <h1>Gatsby is awesome!</h1>
 
 test("Displays the correct title", () => {
-  const { getByTestId } = render(<Title />)
+  const { getByText } = render(<Title />)
   // Assertion
-  expect(getByTestId("hero-title")).toHaveTextContent("Gatsby is awesome!")
+  expect(getByText("Gatsby is awesome!")).toBeInTheDocument()
   // --> Test will pass
 })
