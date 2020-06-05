263449ebcc3247b917c060b346a7f8ca80eb0d05
DomitriusAnthony <domitriusaclark@gmail.com>
2020-06-05 19:59:47 -0400

    trying to sync env variables for search function

--- functions/search.js
+++ functions/search.js
@@ -1,7 +1,7 @@
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
