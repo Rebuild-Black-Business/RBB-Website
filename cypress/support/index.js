d73266119ae87596c24d68944447cf6e5d0dc36c
Jake LeBoeuf <dev@jakeleboeuf.com>
2020-06-04 20:51:39 -0400

    Add an accessibility test

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -15,6 +15,8 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands';
+import 'cypress-axe';
+import '@testing-library/cypress/add-commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
