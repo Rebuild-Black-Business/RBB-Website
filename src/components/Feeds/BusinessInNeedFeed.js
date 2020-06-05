cd2085a1ff3124dc239544a91e6bb02a3b099c0b
Kurt Kemple <kurtiskemple@gmail.com>
2020-06-05 19:49:30 -0400

    add basic schema markup to businesses

--- src/components/Feeds/BusinessInNeedFeed.js
+++ src/components/Feeds/BusinessInNeedFeed.js
@@ -3,28 +3,45 @@ import { StaticQuery, graphql } from 'gatsby';
 
 import { Box } from '@chakra-ui/core';
 
-const SupportingOrgs = data => {
-  return <Box as="pre">{JSON.stringify(data, null, 2)}</Box>;
+const Businesses = data => {
+  return (
+    <Box as="ul">
+      {data.allAirtableBizInNeed.edges.map(({ node: { data } }) => (
+        <Box as="li" itemScope itemType={`http://schema.org/${data.Category}`}>
+          {/* the category in Airtable isn't going to match the schema category, will need to be fixed */}
+          <h2 itemprop="name">{data.Name}</h2>
+          <h4 itemprop="address">{data.Location}</h4>
+          {/* it would be good to get more address details as well */}
+          <span itemprop="email">{data.Email}</span>
+          <a href={data.Donation_Website}>Donate</a>
+        </Box>
+      ))}
+    </Box>
+  );
 };
 
 export default props => (
   <StaticQuery
     query={graphql`
-      query {
-        allAirtableBizInNeed {
-          nodes {
-            data {
-              Category
-              CreatedAt
-              Donation_Website
-              Email
-              Impacted_Business_Name
-              Name
+      query AllApprovedBizInNeed {
+        allAirtableBizInNeed(filter: { data: { Approved: { eq: true } } }) {
+          edges {
+            node {
+              data {
+                Approved
+                Category
+                Donation_Website
+                Email
+                Impacted_Business_Name
+                Location
+                Name
+                Other_Applicable_Links
+              }
             }
           }
         }
       }
     `}
-    render={data => <SupportingOrgs data={data} {...props} />}
+    render={data => <Businesses data={data} {...props} />}
   />
 );
