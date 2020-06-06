d06e92068f2c3df929d8382449d4dd2a5c2ac2f8
brewswain <brandonlee2543@hotmail.com>
2020-06-06 18:09:57 -0400

    Created util function that takes a zipcode as an input, and returns an object with location information of said zipcode, such as City and State.

---
+++ src/utils/zipcodeConversion.js
@@ -0,0 +1,15 @@
+import zipcodes from 'zipcodes';
+
+const zipcodeConversion = zipcode => {
+  try {
+    convertedZipcode = zipcodes.lookup(zipcode);
+    if (!convertedZipcode) throw error;
+    return convertedZipcode;
+  } catch (error) {
+    console.log(
+      'Sorry, zipcode is invalid. Please check your input and try again.'
+    );
+  }
+};
+
+export default zipcodeConversion;
