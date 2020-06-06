b78ee0770c7660250d64b1abd9acd41d9259271a
brewswain <brandonlee2543@hotmail.com>
2020-06-06 18:40:05 -0400

    fixed 'error is not defined' error.

--- src/utils/zipcodeConversion.js
+++ src/utils/zipcodeConversion.js
@@ -3,12 +3,12 @@ import zipcodes from 'zipcodes';
 const zipcodeConversion = zipcode => {
   try {
     let convertedZipcode = zipcodes.lookup(zipcode);
+    let error =
+      'Sorry, zipcode is invalid. Please check your input and try again.';
     if (!convertedZipcode) throw error;
     return convertedZipcode;
   } catch (error) {
-    console.log(
-      'Sorry, zipcode is invalid. Please check your input and try again.'
-    );
+    console.log(error);
   }
 };
 
