736dc8ea5c3e92d97a9d5b97e20efd0f592e746e
Billy Le <lebilly87@gmail.com>
2020-06-07 04:42:23 -0700

    feature/pagination-component: created pagination component and added pagination placeholders to business, allies, and resources pages

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -3,12 +3,20 @@ import React from 'react';
 import { Flex, Heading } from '@chakra-ui/core';
 import Layout from '../components/Layout';
 import ResourceFeed from '../components/Feeds/ResourceFeed';
+import Pagination from '../components/Pagination/Pagination';
 
 export default function Resources() {
   return (
     <Layout>
       <Flex align="center" justify="center" direction="column">
         <Heading>Resources</Heading>
+        <Pagination
+          onPageChanged={pagination => {
+            // @TODO add pagination handler
+          }}
+          totalRecords={70}
+          pageLimit={5}
+        />
         <ResourceFeed />
       </Flex>
     </Layout>
