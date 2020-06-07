e1814ec44e2c53f8d3bc0ea0ebfcef352f77614a
Andrew Chough <andrewchough@gmail.com>
2020-06-06 18:30:23 -0700

    Added global header for desktop and mobile view

--- src/components/PrimaryNav.js
+++ src/components/PrimaryNav.js
@@ -1,16 +1,18 @@
 import React, { useState, useLayoutEffect, forwardRef } from 'react';
 import { useTheme } from '@chakra-ui/core';
-import { Flex, Link, Heading, Box } from '@chakra-ui/core';
+import { Flex, Link, Box, Image } from '@chakra-ui/core';
 import { Link as GatsbyLink } from 'gatsby';
 import VisuallyHidden from '@reach/visually-hidden';
 import { Nav, NavMenu, NavItem, NavLink } from './Nav';
 
 const INITIAL_TOGGLE_STATE = false;
 
-const PrimaryNav = forwardRef(({ menuLinks, ...props }, ref) => {
+const PrimaryNav = forwardRef(({ menuLinks, photoInfo, ...props }, ref) => {
   const [isVisible, setIsVisible] = useState(INITIAL_TOGGLE_STATE);
   const isMedium = useThemeBreakpoint('md');
   const handleToggle = () => setIsVisible(!isVisible);
+  const theme = useTheme();
+  const toUpperCase = text => text.toUpperCase();
 
   // Layout effect prevents a flash of visibility when resizing the screen
   useLayoutEffect(() => {
@@ -18,32 +20,43 @@ const PrimaryNav = forwardRef(({ menuLinks, ...props }, ref) => {
   }, [isMedium]);
 
   return (
+    // Breakpoints follow a [default, 1st breakpoint, 2nd breakpoint] structure.
+    // In this case, padding would be 0 by default, 0 for the 'sm' breakpoint, and 6
+    // for the 'md' breakpoint and above (lg, xl, ...).
+
     <Nav
       ref={ref}
       align="center"
       justify="space-between"
       wrap="wrap"
       aria-label="Primary site navigation"
+      backgroundColor={theme.colors['rbb-black-000']}
+      color={theme.colors['rbb-white']}
+      fontFamily="Arvo"
+      p={[0, 0, 6]}
       {...props}
     >
-      <Flex align="center" mr={5}>
-        <Heading as="h1" size="lg">
-          <Link as={GatsbyLink} to="/">
-            Rebuild Black Businesses
-          </Link>
-        </Heading>
+      <Flex align="center" ml={5} my={[5, 5, 0]}>
+        <Link as={GatsbyLink} to="/">
+          <Image
+            size="16"
+            src="https://images-na.ssl-images-amazon.com/images/I/41hBGDoF8eL._AC_.jpg"
+            alt={photoInfo.alt}
+          />
+        </Link>
       </Flex>
 
-      <Box display={{ sm: 'block', md: 'none' }}>
+      <Box display={['block', 'block', 'none']} mr={{ sm: '5' }}>
         <button onClick={handleToggle}>
           <VisuallyHidden>
             {`${isVisible ? 'Hide' : 'Show'} the navigation menu`}
           </VisuallyHidden>
           <svg
             aria-hidden
-            width="12px"
+            width="30px"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
+            fill={theme.colors['rbb-white']}
           >
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
@@ -52,8 +65,8 @@ const PrimaryNav = forwardRef(({ menuLinks, ...props }, ref) => {
       </Box>
 
       <NavMenu
-        display={{ sm: 'block', md: 'flex' }}
-        width={{ sm: 'full', md: 'auto' }}
+        display={['flex', 'block', 'flex']}
+        width={['auto', 'full', 'auto']}
         alignItems="center"
         justify="flex-end"
         flexGrow={1}
@@ -62,14 +75,18 @@ const PrimaryNav = forwardRef(({ menuLinks, ...props }, ref) => {
         {menuLinks.map((link, index, src) => (
           <NavItem
             key={link.name}
-            mt={{ base: 4, md: 0 }}
-            mr={{
-              base: undefined,
-              md: index !== src.length - 1 ? 6 : undefined,
-            }}
+            p={[6, 6, 0]}
+            border={[
+              `1px solid ${theme.colors['rbb-gray']}`,
+              `1px solid ${theme.colors['rbb-gray']}`,
+              'none',
+            ]}
+            mx={[undefined, undefined, index !== src.length - 1 ? 6 : 8]}
             display="block"
+            fontWeight="bold"
+            textAlign="right"
           >
-            <NavLink to={link.slug}>{link.name}</NavLink>
+            <NavLink to={link.slug}>{toUpperCase(link.name)}</NavLink>
           </NavItem>
         ))}
       </NavMenu>
@@ -89,6 +106,7 @@ function useThemeBreakpoint(breakpoint) {
   const index = sizes.indexOf(breakpoint);
   const theme = useTheme();
   const query = `(min-width: ${theme.breakpoints[index]})`;
+  console.log(query);
   const [matches, setMatches] = useState(
     typeof window !== 'undefined' ? window.matchMedia(query).matches : false
   );
