92ace2a71bc50c866a2f9e45f7a89dae747bd4fe
subject026 <sudobuildspeakers@gmail.com>
2020-06-07 17:37:06 +0100

    added style props to link component

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import { Link as ChakraLink } from '@chakra-ui/core';
+import { Link as ChakraLink, useTheme } from '@chakra-ui/core';
 import { Link as GatsbyLink } from 'gatsby';
 
 function Link(props) {
+  const theme = useTheme();
+  const isBizOrAlly = props.isBizOrAlly ? true : false;
+  console.log('isBiz: ', isBizOrAlly);
+  console.log('visited: ', theme.links.bizOrAlly.color.visited);
   return (
-    <ChakraLink as={GatsbyLink} {...props}>
+    <ChakraLink
+      as={GatsbyLink}
+      {...props}
+      fontFamily={theme.links.font}
+      textDecoration="underline"
+      color={
+        isBizOrAlly
+          ? theme.links.bizOrAlly.color.default
+          : theme.links.default.color.default
+      }
+      _hover={{
+        color: isBizOrAlly
+          ? theme.links.bizOrAlly.color.hover
+          : theme.links.default.color.hover,
+      }}
+      _focus={{
+        color: isBizOrAlly
+          ? theme.links.bizOrAlly.color.focus
+          : theme.links.default.color.focus,
+      }}
+      _visited={{
+        color: isBizOrAlly
+          ? theme.links.bizOrAlly.color.visited
+          : theme.links.default.color.visited,
+      }}
+    >
       {props.children}
     </ChakraLink>
   );
