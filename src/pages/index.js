6580fe909c72432b48be252c94d97573ec23b6c2
Tristan payne <tpayne@emfluence.com>
2020-06-08 17:02:04 -0500

    feat: add ContentBlock component
    Adds an update index.js with examples

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-import { Flex, Heading } from '@chakra-ui/core';
+import { Box, Flex, Heading, Text, theme } from '@chakra-ui/core';
 import { Helmet } from 'react-helmet';
 import Layout from '../components/Layout';
+import ContentBlock from '../components/ContentBlock';
+import PrimaryButton from '../components/Buttons/PrimaryButton';
 
 export default () => {
   return (
@@ -14,6 +16,77 @@ export default () => {
         <Heading size="2xl" fontFamily="Arvo">
           Home
         </Heading>
+        <Heading>Center Content</Heading>
+        <ContentBlock
+          layout="full"
+          imageSource="url('https://placehold.it/1920x300')"
+          backgroundColor="tomato"
+        >
+          <Box py={16}>
+            <Heading>Business Owners, We're here for you.</Heading>
+            <Text>Our mission is to connect...</Text>
+            <Text>If you need additional assistance,...</Text>
+            <PrimaryButton>Click Me</PrimaryButton>
+          </Box>
+        </ContentBlock>
+
+        <ContentBlock
+          layout="right"
+          imageSource="url('https://placehold.it/1920x300')"
+          backgroundColor="#000"
+          backgroundMode="fade"
+        >
+          <Box py={16} color="white">
+            <Heading>Business Owners, We're here for you.</Heading>
+            <Text>Our mission is to connect...</Text>
+            <Text>If you need additional assistance,...</Text>
+            <PrimaryButton>Click Me</PrimaryButton>
+          </Box>
+        </ContentBlock>
+
+        <Heading>Right Side Content</Heading>
+        <ContentBlock
+          layout="right"
+          imageSource="url('https://placehold.it/1920x300')"
+          backgroundColor="#ff00ff"
+          backgroundMode="tint"
+        >
+          <Box py={16} color="white">
+            <Heading>Business Owners, We're here for you.</Heading>
+            <Text>Our mission is to connect...</Text>
+            <Text>If you need additional assistance,...</Text>
+            <PrimaryButton>Click Me</PrimaryButton>
+          </Box>
+        </ContentBlock>
+
+        <Heading>Left Side Content</Heading>
+        <ContentBlock
+          layout="left"
+          imageSource="url('https://placehold.it/1920x300')"
+          backgroundColor="#000"
+          backgroundMode="fade"
+        >
+          <Box py={16} color="white">
+            <Heading>Business Owners, We're here for you.</Heading>
+            <Text>Our mission is to connect...</Text>
+            <Text>If you need additional assistance,...</Text>
+            <PrimaryButton>Click Me</PrimaryButton>
+          </Box>
+        </ContentBlock>
+
+        <ContentBlock
+          layout="left"
+          imageSource="url('https://placehold.it/1920x300')"
+          backgroundColor="#ff00ff"
+          backgroundMode="tint"
+        >
+          <Box py={16} color="white">
+            <Heading>Business Owners, We're here for you.</Heading>
+            <Text>Our mission is to connect...</Text>
+            <Text>If you need additional assistance,...</Text>
+            <PrimaryButton>Click Me</PrimaryButton>
+          </Box>
+        </ContentBlock>
       </Flex>
     </Layout>
   );
