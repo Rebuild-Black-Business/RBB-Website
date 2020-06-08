48d63a356f73747b46b7b4758118afb48145123c
Ryan Harris <harrisryan1@gmail.com>
2020-06-07 20:38:39 -0400

    chore: move function to locationUtils

--- src/utils/locationUtils.js
+++ src/utils/locationUtils.js
@@ -12,3 +12,15 @@ export function getLocationZip(locationString) {
     return [parseInt(locationString)];
   }
 }
+
+export function zipcodeConversion(zipcode) {
+  try {
+    let convertedZipcode = zipcodes.lookup(zipcode);
+    let error =
+      'Sorry, zipcode is invalid. Please check your input and try again.';
+    if (!convertedZipcode) throw error;
+    return convertedZipcode;
+  } catch (error) {
+    console.log(error);
+  }
+}
