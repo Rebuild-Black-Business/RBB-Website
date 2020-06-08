89f41abbdef9b34c3e3753f7df38c797ef3c2cb2
Mike Bifulco <mbifulco@live.com>
2020-06-08 15:11:11 -0400

    hero as stack, add heading styles

--- src/components/PageHero.js
+++ src/components/PageHero.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import {
-  AspectRatioBox,
-  Box,
-  Flex,
-  Image,
-  Heading,
-  Text,
-  Stack,
-  useTheme,
-} from '@chakra-ui/core';
+import { Heading, Stack, useTheme } from '@chakra-ui/core';
 
 const PageHero = ({ title, subtitle }) => {
   const theme = useTheme();
+
   return (
-    <Box as="header" display="flex" flexDirection="column" alignItems="center">
-      <Heading as="h1" size="2xl">
+    <Stack
+      as="header"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      padding="1.5rem 0"
+    >
+      <Heading
+        as="h1"
+        size="2xl"
+        textTransform="uppercase"
+        fontFamily={theme.fonts['heading-slab']}
+        fontWeight={theme.fontWeights.bold}
+      >
         {title}
       </Heading>
       <Heading as="h2" size="16pt" width={['100%', '80%']}>
         {subtitle}
       </Heading>
-    </Box>
+    </Stack>
   );
 };
 
