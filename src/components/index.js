b4283f5006162e6848a74f74dec748e2534930f5
Mike Bifulco <mbifulco@live.com>
2020-06-08 15:10:52 -0400

    organize components a bit

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
+export { default as BusinessFeed } from './Feeds/BusinessFeed';
 export { default as Layout } from './Layout';
-export { default as SEO } from './SEO/SEO';
 export { default as PageHero } from './PageHero';
+export { default as Pagination } from './Pagination/Pagination';
+export { default as SEO } from './SEO/SEO';
