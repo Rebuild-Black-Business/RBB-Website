2266335527261b30b70c1f9db93a6dc8a2615128
Ryan Harris <harrisryan1@gmail.com>
2020-06-08 20:21:30 -0400

    fix: move filtering to useMemo

--- src/components/Feeds/AllyFeed.js
+++ src/components/Feeds/AllyFeed.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useMemo } from 'react';
 import { StaticQuery, graphql } from 'gatsby';
 import {
   Box,
@@ -40,10 +40,30 @@ const ModalForm = ({ isOpen, onClose, title }) => (
 
 const AllyFeed = props => {
   const [allAllies] = useState(props.data.allAirtableAllies.nodes);
+  const [allies, setAllies] = useState(allAllies);
   const { onOpen, isOpen, onClose } = useDisclosure();
   const focusRef = useRef();
   const theme = useTheme();
   const { skill: skillFilter, location: locationFilter } = props.filters;
+
+  useMemo(() => {
+    const filteredAllies = allAllies
+      .filter(ally => {
+        if (skillFilter === '') return ally;
+        return ally.data['Speciality'] === skillFilter;
+      })
+      .filter(ally => {
+        if (locationFilter === '') return ally;
+
+        const zipcodesInRadius = getZipcodesByRadius(locationFilter, 25);
+
+        if (zipcodesInRadius.length === 0) return ally;
+
+        return zipcodesInRadius.includes(ally.data['Zip_Code']);
+      });
+
+    setAllies(filteredAllies);
+  }, [locationFilter, skillFilter, allAllies]);
 
   return (
     <Box
@@ -56,101 +76,87 @@ const AllyFeed = props => {
           spacing={theme.spacing.med}
           paddingBottom={theme.spacing.lg}
         >
-          {allAllies
-            .filter(ally => {
-              if (skillFilter === '') return ally;
-              return ally.data['Speciality'] === skillFilter;
-            })
-            .filter(ally => {
-              if (locationFilter === '') return ally;
-
-              const zipcodesInRadius = getZipcodesByRadius(locationFilter, 25);
-
-              if (zipcodesInRadius.length === 0) return ally;
-
-              return zipcodesInRadius.includes(ally.data['Zip_Code']);
-            })
-            .map((allies, index) => {
-              if (index === 4)
-                return (
-                  <>
-                    <CardWrapper
-                      gridColumn={[null, null, 'span 2']}
-                      pr={theme.spacing.lg}
-                      pos="relative"
-                    >
-                      <Image
-                        cloudName="rebuild-black-business"
-                        publicId="assets/ally-sign-up"
-                        transforms={{
-                          fetchFormat: 'auto',
-                          quality: 'auto',
-                        }}
-                        objectFit="cover"
-                        pos="absolute"
-                        zIndex="-1"
-                        w="100%"
-                        h="100%"
-                        top="0"
-                        left="0"
-                      />
-                      <CardContent
-                        color={theme.colors['rbb-white']}
-                        display="flex"
-                        flexDirection="column"
-                      >
-                        <CardHeading
-                          fontFamily={theme.fonts['heading-slab']}
-                          textTransform="uppercase"
-                          fontSize={theme.fontSizes.xl}
-                          lineHeight="1"
-                          overflowWrap="break-word"
-                          wordWrap="break-word"
-                          wordBreak="break-word"
-                          hyphens="auto"
-                        >
-                          Sign up to be an Ally
-                        </CardHeading>
-                        <CardText
-                          as="p"
-                          fontFamily={theme.fonts.heading}
-                          fontSize={theme.fontSizes.base}
-                        >
-                          Use your skills to assist Black-owned businesses to
-                          return to business and stay afloat while operating.
-                          Sign added to a private directory of Allies shared
-                          with Black business owners.
-                        </CardText>
-                        <Button
-                          variant="cta"
-                          mt={theme.spacing.base}
-                          onClick={onOpen}
-                          ref={focusRef}
-                        >
-                          Share Your Skills
-                        </Button>
-                      </CardContent>
-                    </CardWrapper>
-
-                    <AllyCard
-                      key={index}
-                      name={allies.data.Name}
-                      email={allies.data.Email}
-                      specialty={allies.data.Speciality}
-                      location={allies.data.Zip_Code}
-                    />
-                  </>
-                );
+          {allies.map((allies, index) => {
+            if (index === 4)
               return (
-                <AllyCard
-                  key={index}
-                  name={allies.data.Name}
-                  email={allies.data.Email}
-                  specialty={allies.data.Speciality}
-                  location={allies.data.Zip_Code}
-                />
+                <>
+                  <CardWrapper
+                    gridColumn={[null, null, 'span 2']}
+                    pr={theme.spacing.lg}
+                    pos="relative"
+                  >
+                    <Image
+                      cloudName="rebuild-black-business"
+                      publicId="assets/ally-sign-up"
+                      transforms={{
+                        fetchFormat: 'auto',
+                        quality: 'auto',
+                      }}
+                      objectFit="cover"
+                      pos="absolute"
+                      zIndex="-1"
+                      w="100%"
+                      h="100%"
+                      top="0"
+                      left="0"
+                    />
+                    <CardContent
+                      color={theme.colors['rbb-white']}
+                      display="flex"
+                      flexDirection="column"
+                    >
+                      <CardHeading
+                        fontFamily={theme.fonts['heading-slab']}
+                        textTransform="uppercase"
+                        fontSize={theme.fontSizes.xl}
+                        lineHeight="1"
+                        overflowWrap="break-word"
+                        wordWrap="break-word"
+                        wordBreak="break-word"
+                        hyphens="auto"
+                      >
+                        Sign up to be an Ally
+                      </CardHeading>
+                      <CardText
+                        as="p"
+                        fontFamily={theme.fonts.heading}
+                        fontSize={theme.fontSizes.base}
+                      >
+                        Use your skills to assist Black-owned businesses to
+                        return to business and stay afloat while operating. Sign
+                        added to a private directory of Allies shared with Black
+                        business owners.
+                      </CardText>
+                      <Button
+                        variant="cta"
+                        mt={theme.spacing.base}
+                        onClick={onOpen}
+                        ref={focusRef}
+                      >
+                        Share Your Skills
+                      </Button>
+                    </CardContent>
+                  </CardWrapper>
+
+                  <AllyCard
+                    key={index}
+                    name={allies.data.Name}
+                    email={allies.data.Email}
+                    specialty={allies.data.Speciality}
+                    location={allies.data.Zip_Code}
+                  />
+                </>
               );
-            })}
+            return (
+              <AllyCard
+                key={index}
+                name={allies.data.Name}
+                email={allies.data.Email}
+                specialty={allies.data.Speciality}
+                location={allies.data.Zip_Code}
+              />
+            );
+          })}
         </SimpleGrid>
       ) : (
         <Box as="pre">No results...</Box>
