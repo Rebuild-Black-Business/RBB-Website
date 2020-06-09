2d1ad44fe59db778f07cf83c03f18aa019f8cb05
Lizzie Davison <lizzie@hubblehq.com>
2020-06-09 22:57:46 +0100

    Add in algolia search hook
    Added in algolia search hook and hooked it up to the pagination, the
    pagination is no longer working so pushing this up for someone to take a
    look at :).

--- src/templates/businesses.js
+++ src/templates/businesses.js
@@ -1,11 +1,65 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import algoliasearch from 'algoliasearch/lite';
 
 import { graphql, navigate } from 'gatsby';
 import { Flex } from '@chakra-ui/core';
 
 import { PageHero, Layout, BusinessFeed, Pagination } from '../components';
 
+const client = algoliasearch('LRWZTMM362', 'b1556413e51961cacf7dbb37f22f4094');
+const index = client.initIndex('businesses');
+
+const LOADING_STATE = {
+  NONE: 'none',
+  INITIAL: 'intial',
+  SEARCHING: 'searching',
+};
+
+function useAlgoliaSearch() {
+  const [results, setResults] = useState([]);
+  const [loadingState, setLoadingState] = useState(LOADING_STATE.INITIAL);
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalResults, setTotalResults] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    async function getBusinesses() {
+      try {
+        const algoliaResponse = await index.search('', {
+          page: 1,
+        });
+
+        setResults(algoliaResponse.hits);
+        setTotalPages(algoliaResponse.nbPages);
+        setTotalResults(algoliaResponse.nbHits);
+        setCurrentPage(algoliaResponse.page);
+        setLoadingState(LOADING_STATE.NONE);
+      } catch (e) {
+        console.log('error searching', e);
+      }
+    }
+
+    getBusinesses();
+  }, []);
+
+  return {
+    results,
+    totalPages,
+    totalResults,
+    currentPage,
+    loadingState,
+  };
+}
+
 export default function Businesses(data) {
+  const {
+    currentPage,
+    loadingState,
+    results,
+    totalPages,
+    totalResults,
+  } = useAlgoliaSearch();
+
   const pageSubtitle = (
     <p>
       These business owners have been impacted during the protests. Your support
@@ -35,9 +89,8 @@ export default function Businesses(data) {
               }`
             )
           }
-          currentPage={data.pageContext.page}
-          totalRecords={data.pageContext.totalRecords}
-          pageLimit={data.pageContext.itemsPerPage}
+          currentPage={currentPage}
+          totalPages={totalPages}
         />
       </Flex>
     </Layout>
