5e3d29736c8a809c922e40fdc0196c5061646399
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-06-08 19:07:30 -0600

    Add modal, image, and fallback image functionality.

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,4 +1,4 @@
-import React, { forwardRef } from 'react';
+import React, { forwardRef, useRef } from 'react';
 import PropTypes from 'prop-types';
 import {
   CardWrapper,
@@ -6,11 +6,53 @@ import {
   CardContent,
   CardHeading,
   CardText,
-  CardButtonGroup,
   CardButton,
+  CardButtonGroup,
 } from './Card';
-import { Link, Text } from '@chakra-ui/core';
+import {
+  Text,
+  Box,
+  Icon,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useTheme,
+  useDisclosure,
+} from '@chakra-ui/core';
 import { zipcodeConversion } from '../utils/locationUtils';
+
+import Button from './Button';
+import Link from './Link';
+
+const toCamelCase = string => {
+  string = string
+    .toLowerCase()
+    .replace(/(?:(^.)|([-_\s]+.))/g, function (match) {
+      return match.charAt(match.length - 1).toUpperCase();
+    });
+  return string.charAt(0).toLowerCase() + string.substring(1);
+};
+
+// @TODO :: Add proper content to this modal. Probably pull this out into its own file seeing as its going to be a form
+const ModalForm = ({ isOpen, onClose, title }) => (
+  <Modal isOpen={isOpen} onClose={onClose}>
+    <ModalOverlay />
+    <ModalContent>
+      <ModalHeader pr="3rem">{title}</ModalHeader>
+      <ModalCloseButton />
+      <ModalBody>Message for reporting or updating here</ModalBody>
+      <ModalFooter>
+        <Button variantColor="blue" m={3} onClick={onClose}>
+          Close
+        </Button>
+      </ModalFooter>
+    </ModalContent>
+  </Modal>
+);
 
 // TODO: Replace with real fallback images for each category.
 // This should all probably be defined in the database somewhere, eh?
@@ -18,42 +60,42 @@ const categoryData = {
   entertainment: {
     label: 'Entertainment',
     image: {
-      src: 'https://source.unsplash.com/random',
+      src: 'assets/business-entertainment-option',
       alt: 'Id facilisis dictum consequat sit orci.',
     },
-    buttonText: 'Learn more',
+    buttonText: 'Explore',
   },
   foodAndBeverage: {
     label: 'Food and Beverage',
     image: {
-      src: 'https://source.unsplash.com/random',
+      src: 'assets/business-food-beverage',
       alt: 'Id facilisis dictum consequat sit orci.',
     },
     buttonText: 'Order',
   },
-  healthAndWellness: {
-    label: 'Health and Wellness',
-    image: {
-      src: 'https://source.unsplash.com/random',
-      alt: 'Id facilisis dictum consequat sit orci.',
-    },
-    buttonText: 'Learn more',
-  },
   professionalServices: {
     label: 'Professional Services',
     image: {
-      src: 'https://source.unsplash.com/random',
+      src: 'assets/business-services',
       alt: 'Id facilisis dictum consequat sit orci.',
     },
-    buttonText: 'Contact',
+    buttonText: 'Shop',
   },
   retail: {
     label: 'Retail',
     image: {
-      src: 'https://source.unsplash.com/random',
+      src: 'assets/business-retail',
       alt: 'Id facilisis dictum consequat sit orci.',
     },
     buttonText: 'Shop',
+  },
+  healthAndWellness: {
+    label: 'Health and Wellness',
+    image: {
+      src: 'assets/business-health',
+      alt: 'Id facilisis dictum consequat sit orci.',
+    },
+    buttonText: 'Explore',
   },
 };
 
@@ -89,9 +131,10 @@ const ResultCard = forwardRef(
     },
     ref
   ) => {
-    const hasFallbackImage =
-      category && Object.keys(categoryData).includes(category);
-    const hasImage = !!(imageSrc || hasFallbackImage);
+    const { onOpen, isOpen, onClose } = useDisclosure();
+    const { reportRef, updateRef } = useRef();
+    const theme = useTheme();
+    const catVar = toCamelCase(category);
 
     // I'm not sure how categories are going to work, so this probaably needs to
     // change. Also unsure how we're going to handle the schema category on the
@@ -101,47 +144,102 @@ const ResultCard = forwardRef(
     const zipInfo = zipcodeConversion(location);
     const formattedCity = zipInfo ? `${zipInfo.city}, ${zipInfo.state}` : null;
 
+    let img;
+    if (imageSrc !== null) {
+      img = <CardImage src={imageSrc} alt={imageAlt} />;
+    } else {
+      img = (
+        <CardImage
+          isRBBImage
+          publicId={categoryData[catVar].image.src}
+          alt={categoryData[catVar].image.alt}
+        />
+      );
+    }
+
     return (
-      <CardWrapper
-        ref={ref}
-        {...props}
-        // TODO: Use real theme colors per the design
-        border="1px solid gray"
-        itemScope
-        // TODO: the category in Airtable isn't going to match the schema
-        // category, will need to be fixed. Omitting for now.
-        // itemType={`http://schema.org/${categoryLabel}`}
-      >
-        {hasImage && (
-          <CardImage
-            src={imageSrc || categoryData[category].image.src}
-            alt={imageSrc ? imageAlt : categoryData[category].image.alt}
-          />
-        )}
-        <CardContent
-          // TODO: Use real theme colors per the design
-          bg={hasImage ? 'white' : '#555'}
-          color={hasImage ? undefined : 'white'}
+      <>
+        <CardWrapper
+          ref={ref}
+          {...props}
+          itemScope
+          // TODO: the category in Airtable isn't going to match the schema
+          // category, will need to be fixed. Omitting for now.
+          // itemType={`http://schema.org/${categoryLabel}`}
         >
-          <CardHeading itemprop="name">{name}</CardHeading>
-          {category && <CardText as="span">{categoryLabel}</CardText>}
-          {description && <CardText as="p">{description}</CardText>}
-          {formattedCity && <CardText as="p">{formattedCity}</CardText>}
-          <CardButtonGroup>
-            <CardButton as="a" href={websiteUrl}>
-              {(category && categoryData[category]?.buttonText) || 'Learn More'}
-            </CardButton>
-            {donationUrl && (
-              <CardButton href={donationUrl} as="a">
-                Donate
-              </CardButton>
+          {img}
+          <CardContent
+            // TODO: Use real theme colors per the design
+            bg="#DEDEDA"
+            color={theme.colors['rbb-black-100']}
+            fontFamily={theme.fonts.heading}
+            display="flex"
+            flexDirection="column"
+          >
+            <CardHeading itemprop="name" fontSize="lg" fontWeight="normal">
+              {name}
+            </CardHeading>
+            {category && (
+              <CardText
+                as="span"
+                fontSize="sm"
+                color={theme.colors['rbb-gray']}
+                mb={theme.spacing.xs}
+              >
+                {categoryLabel}
+              </CardText>
             )}
-          </CardButtonGroup>
-          <Text as="small" fontSize="sm" fontStyle="italic" mt={3}>
-            <Link href="#">Report or update</Link>
-          </Text>
-        </CardContent>
-      </CardWrapper>
+            {description && (
+              <CardText as="p" isTruncated>
+                {description}
+              </CardText>
+            )}
+            {formattedCity && <CardText as="p">{formattedCity}</CardText>}
+            <CardButtonGroup marginTop="auto" paddingTop={theme.spacing.base}>
+              <CardButton as="a" href={websiteUrl}>
+                {(category && categoryData[catVar]?.buttonText) || 'Learn More'}
+              </CardButton>
+              {donationUrl && (
+                <CardButton href={donationUrl} as="a">
+                  Donate
+                </CardButton>
+              )}
+            </CardButtonGroup>
+            <Box paddingTop={theme.spacing.base}>
+              <Text as="small" fontSize="sm" fontStyle="italic" mt={3} isInline>
+                <Icon
+                  name="flag"
+                  color={theme.colors['rbb-gray']}
+                  mr={theme.spacing.xs}
+                />
+                <Link
+                  as="button"
+                  variant="cta"
+                  onClick={onOpen}
+                  ref={reportRef}
+                >
+                  Report
+                </Link>{' '}
+                or{' '}
+                <Link
+                  as="button"
+                  variant="cta"
+                  onClick={onOpen}
+                  ref={updateRef}
+                >
+                  update
+                </Link>
+              </Text>
+            </Box>
+          </CardContent>
+        </CardWrapper>
+
+        <ModalForm
+          isOpen={isOpen}
+          title={`Report or Update the listing for "${name}"`}
+          onClose={onClose}
+        />
+      </>
     );
   }
 );
@@ -151,7 +249,7 @@ ResultCard.propTypes = {
   category: PropTypes.string,
   name: PropTypes.string.isRequired,
   description: PropTypes.string,
-  location: PropTypes.number,
+  location: PropTypes.string,
   websiteUrl: PropTypes.string.isRequired,
   donationUrl: PropTypes.string,
   imageSrc: PropTypes.string,
