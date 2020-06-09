ea23005eaf2be24a7e470e6479c9cea89e16c306
HAINES-DESKTOP\richa <richardkhaines@hotmail.co.uk>
2020-06-09 22:41:30 +0200

    removed my clashing changes to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -47,7 +47,6 @@ export default function About() {
             marginBottom={['1.125rem', '1.125rem', 0, 0]}
             marginLeft={[0, 0, '5%', '15%', '15%', '30%']}
             marginRight={[0, 0, '5%', '20%', '22%', '35%']}
-            maxW="1200px"
           >
             <Content
               heading="MISSION"
@@ -89,7 +88,6 @@ export default function About() {
             rowGap="24px"
             templateColumns="repeat(auto-fit, minmax(350px, 1fr))"
             w="100%"
-            maxW="1400px"
             direction={['column', 'column', 'row', 'row']}
             paddingTop="2rem"
             paddingBottom="2rem"
