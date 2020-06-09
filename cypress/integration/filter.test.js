490a456a222d12140c6b23a10f1f18b07afe9134
Jake LeBoeuf <dev@jakeleboeuf.com>
2020-06-09 13:41:27 -0400

    Add some tests to make sure our filtering results show a form when there are no results

---
+++ cypress/integration/filter.test.js
@@ -0,0 +1,47 @@
+/// <reference types="Cypress" />
+
+describe('Business Screen', () => {
+  const LOCATION = 'San Francisco';
+
+  beforeEach(() => {
+    cy.visit('/businesses');
+  });
+
+  it('Filtering Businesses will show a Signup form when there are no results', () => {
+    // Fill and Submit the form
+    cy.get('#need').select('All');
+    cy.get('#type').select('Retail');
+    cy.get('#location').click().type(`${LOCATION}{enter}`);
+
+    // Check for the things we want to see
+    cy.get("h2[data-testid='heading']").contains(
+      /Sorry, no allies matched this search/i
+    );
+    cy.get("h2[data-testid='subheading']").as('subheading');
+    cy.get('@subheading').contains(LOCATION);
+
+    // TODO: Look for some form
+  });
+});
+
+describe('Allies Screen', () => {
+  const ZIP = '29407';
+  beforeEach(() => {
+    cy.visit('/allies');
+  });
+
+  it('Filtering Allies will show a Signup form when there are no results', () => {
+    // Fill and Submit the form
+    cy.get('#skill').select('Tech');
+    cy.get('#location').click().type(`${ZIP}{enter}`);
+
+    // Check for the things we want to see
+    cy.get("h2[data-testid='heading']").contains(
+      /Sorry, no allies matched this search/i
+    );
+    cy.get("h2[data-testid='subheading']").as('subheading');
+    cy.get('@subheading').contains(ZIP);
+
+    // TODO: Look for some form
+  });
+});
