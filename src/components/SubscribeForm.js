6906738c0e8a018a217f22f660805fb63cf198f5
Jordan Andree <jordanandree@gmail.com>
2020-06-10 09:59:58 -0700

    Add initial subscribe form component scaffold

---
+++ src/components/SubscribeForm.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import { FormControl, FormLabel, Input, Stack } from '@chakra-ui/core';
+import Button from './Button';
+
+const SubscribeForm = () => {
+  const formAction =
+    'https://rebuildblackbusiness.us10.list-manage.com/subscribe/post?u=a5fccdceb916e00a46d603ed8&amp;id=992a06eec8';
+
+  return (
+    <form action={formAction} method="post" noValidate>
+      <Stack spacing={8}>
+        <FormControl isRequired>
+          <FormLabel htmlFor="mce-EMAIL">Email Address</FormLabel>
+          <Input type="email" name="EMAIL" id="mce-EMAIL" />
+        </FormControl>
+
+        <FormControl isRequired>
+          <FormLabel htmlFor="mce-FNAME">First Name</FormLabel>
+          <Input type="text" name="FNAME" id="mce-FNAME" />
+        </FormControl>
+
+        <FormControl>
+          <FormLabel htmlFor="mce-LNAME">Last Name</FormLabel>
+          <Input type="text" name="LNAME" id="mce-LNAME" />
+        </FormControl>
+
+        <div
+          style={{ position: 'absolute', left: '-5000px' }}
+          aria-hidden="true"
+        >
+          <input
+            type="text"
+            name="b_a5fccdceb916e00a46d603ed8_992a06eec8"
+            tabIndex="-1"
+            defaultValue=""
+          />
+        </div>
+
+        <Button type="submit" name="subscribe">
+          Subscribe
+        </Button>
+      </Stack>
+    </form>
+  );
+};
+
+export default SubscribeForm;
