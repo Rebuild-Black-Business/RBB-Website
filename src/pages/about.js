d740f5e7f71c031daf7323fad0ce603ea98716fd
Neil Shapiro <nshapiro91@gmail.com>
2020-06-10 17:35:44 -0500

    Remove additional spacing

--- src/pages/about.js
+++ src/pages/about.js
@@ -61,19 +61,8 @@ export default function About() {
             direction="column"
             align="center"
             justify="center"
-            // alignItems="flex-start"
-            marginTop={[
-              theme.spacing.base,
-              theme.spacing.base,
-              theme.spacing.md,
-              theme.spacing.md,
-            ]}
-            marginBottom={[
-              '1.125rem',
-              '1.125rem',
-              theme.spacing.md,
-              theme.spacing.md,
-            ]}
+            marginTop={[theme.spacing.base, theme.spacing.base, 0, 0]}
+            marginBottom={['1.125rem', '1.125rem', 0, 0]}
             marginLeft={[0, 0, '5%', '15%', '15%', '30%']}
             marginRight={[0, 0, '5%', '20%', '22%', '35%']}
           >
