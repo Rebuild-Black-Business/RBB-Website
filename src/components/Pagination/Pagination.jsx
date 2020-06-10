cd14c41d6471fc92532bf5c64ec12f0c18ac66c2
Juan Rios <juan.rios.28@gmail.com>
2020-06-10 14:41:33 -0400

    fix for pagination not having focus

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,9 +1,9 @@
-import React, { useMemo, useState } from 'react';
 import { Button, Flex, PseudoBox, useTheme } from '@chakra-ui/core';
-import PaginationArrow from '../Svgs/PaginationArrow';
-import { range } from '../../utils/common';
 import PropTypes from 'prop-types';
+import React, { useMemo, useState } from 'react';
 import useMedia from 'react-use/lib/useMedia';
+import { range } from '../../utils/common';
+import PaginationArrow from '../Svgs/PaginationArrow';
 
 const PLACEHOLDER = '...';
 
@@ -149,6 +149,7 @@ function Pagination({ onPageChanged, totalRecords, pageLimit, ...props }) {
             fontSize={theme.fontSizes.lg}
             fontWeight={theme.fontWeights.bold}
             cursor="pointer"
+            _focus={{ bg: !isActivePage && theme.colors['rbb-lightgray'] }}
             _hover={{ bg: !isActivePage && theme.colors['rbb-lightgray'] }}
             onClick={handleClick}
             title={`Go to page ${page}`}
