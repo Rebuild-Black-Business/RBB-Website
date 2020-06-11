dd3354d2f2aa53b3ac2b7ce8a63bc00d944ab248
shapirone <nshapiro91@gmail.com>
2020-06-10 23:12:57 -0500

    Update src/components/Filters/AllyFilter.jsx
    Co-authored-by: Dominic Magnifico <dominicmagnifico@gmail.com>

--- src/components/Filters/AllyFilter.jsx
+++ src/components/Filters/AllyFilter.jsx
@@ -91,7 +91,7 @@ function BusinessFilter(props) {
             value={location}
             id="location"
             type="text"
-            placeholder="Denver or 80219"
+            placeholder="80219"
             onChange={event => setLocation(event.currentTarget.value)}
             onKeyPress={event => {
               if (event.key === 'Enter') {
