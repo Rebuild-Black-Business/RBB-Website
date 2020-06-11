712198e85b411e673ca0db8edad3a4ee0d610ca0
Dominic Magnifico <dominicmagnifico@gmail.com>
2020-06-10 20:48:03 -0600

    Ensure the noresultscard has the proper forms.

--- src/components/Cards/NoResultsCard.js
+++ src/components/Cards/NoResultsCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Box, Heading, Text, useTheme } from '@chakra-ui/core';
+import SubmitAlly from '../Forms/SubmitAlly';
+import SubmitBusiness from '../Forms/SubmitBusiness';
 
 const NoResultsCard = ({ type }) => {
   const theme = useTheme();
@@ -21,22 +23,37 @@ const NoResultsCard = ({ type }) => {
         Sorry, No {type} Matched This Search.
       </Heading>
       {type === 'allies' && (
-        <Text
-          data-testid="subheading"
-          my={theme.spacing.base}
-          textAlign="center"
-        >
-          {/* TODO: the "your area" copy needs to be dynamically generated based on filters */}
-          We’d love to have Allies in your area. If you’ve got skills to assist
-          Black-owned businesses, sign up to be added.
-        </Text>
+        <>
+          <Text
+            data-testid="subheading"
+            my={theme.spacing.base}
+            textAlign="center"
+          >
+            {/* TODO: the "your area" copy needs to be dynamically generated based on filters */}
+            We’d love to have Allies in your area. If you’ve got skills to
+            assist Black-owned businesses, sign up to be added.
+          </Text>
+          <SubmitAlly />
+        </>
       )}
 
-      {/*
-        TODO: probably need a form embed ID/component/prop if the
-        forms are different between business and allies
-      */}
-      <pre>TODO: embed Airtable form here</pre>
+      {type === 'businesses' && (
+        <>
+          <Text
+            data-testid="subheading"
+            my={theme.spacing.base}
+            textAlign="center"
+          >
+            {/* TODO: the "your area" copy needs to be dynamically generated based on filters */}
+            We’d love to support Black-owned businesses in your city. Is there a
+            Black-owned business we should know about? Register a business below
+            and we’ll add them to our public directory. If they are in urgent
+            need, include a donation link.
+          </Text>
+
+          <SubmitBusiness />
+        </>
+      )}
     </Box>
   );
 };
