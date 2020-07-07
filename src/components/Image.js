1dadfc95b7c1cdc78fd8230f552c0f35b3b78175
DomitriusAnthony <domitriusaclark@gmail.com>
2020-07-07 08:04:30 -0400

    Upgraded use-cloudinary to v3 and updated hook usage

--- src/components/Image.js
+++ src/components/Image.js
@@ -6,20 +6,20 @@ import ImageSkeleton from './Loading/ImageSkeleton';
 export default function Image(props) {
   const { transforms, publicId, alt, src } = props;
 
-  const { getImage, data, status, error } = useImage({
-    cloud_name: 'rebuild-black-business',
+  const { generateUrl, url, status, error } = useImage({
+    cloudName: 'rebuild-black-business',
   });
 
   /* eslint-disable react-hooks/exhaustive-deps */
   React.useEffect(() => {
-    getImage({
-      public_id: publicId,
-      transform_options: {
+    generateUrl({
+      publicId,
+      transformations: {
         ...transforms,
+        dpr: 2,
         // fetch: 'auto' is applied internally
         // crop: 'scale' is applied automatically when a width or height is supplied
-        quality: 'auto',
-        dpr: 2,
+        // quality: 'auto' is applied automatically
       },
     });
   }, [publicId]);
@@ -30,5 +30,5 @@ export default function Image(props) {
   // @TODO :: Deliver custom Error UI if needed
   if (status === 'error') return <p>{error.message}</p>;
 
-  return <ChakraImage {...props} src={data || src} alt={alt} />;
+  return <ChakraImage {...props} src={url || src} alt={alt} />;
 }
