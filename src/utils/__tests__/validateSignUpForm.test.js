f3a310c866c7afb434893f9108681c811643ae3b
Brian Hanson <brian@brianhanson.net>
2020-11-15 14:03:42 -0600

    Adds basic form validation

---
+++ src/utils/__tests__/validateSignUpForm.test.js
@@ -0,0 +1,48 @@
+import { validateSignUpForm } from '../validateSignUpForm';
+
+describe('validateSignUpForm', () => {
+  describe('website field', () => {
+    it('should be required', () => {
+      const data = {
+        website: null,
+      };
+
+      expect(validateSignUpForm(data)).toMatchObject({
+        website: 'This field is required.',
+      });
+    });
+
+    it('should allow mailto: links', () => {
+      const data = {
+        website: 'mailto:example@example.com',
+      };
+
+      expect(validateSignUpForm(data)).toMatchObject({});
+    });
+    it('should allow https: links', () => {
+      const data = {
+        website: 'https://example.com',
+      };
+
+      expect(validateSignUpForm(data)).toMatchObject({});
+    });
+
+    it('should allow http: links', () => {
+      const data = {
+        website: 'http://example.com',
+      };
+
+      expect(validateSignUpForm(data)).toMatchObject({});
+    });
+
+    it('should not links without protocol', () => {
+      const data = {
+        website: 'www.example.com',
+      };
+
+      expect(validateSignUpForm(data)).toMatchObject({
+        website: 'Please enter a valid URL',
+      });
+    });
+  });
+});
---
+++ src/utils/validateSignUpForm.js
@@ -0,0 +1,23 @@
+export const validateSignUpForm = formData => {
+  return Object.keys(formData).reduce((acc, key) => {
+    const value = formData[key];
+
+    // All fields are required, so if this is false-y, add an error
+    if (value === null) {
+      // Then push this error onto the array
+      acc[key] = 'This field is required.';
+    }
+
+    if (key === 'website') {
+      // taken and slightly adjusted from yup library https://github.com/jquense/yup/blob/15a0f43007d2468d90a4917101d63e3d335e04b3/src/string.js#L9
+      // eslint-disable-next-line
+      let rUrl = /^((https?|mailto):)?\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i;
+
+      if (value && !value.match(rUrl)) {
+        acc[key] = 'Please enter the full URL including http:// or https://';
+      }
+    }
+
+    return acc;
+  }, {});
+};
